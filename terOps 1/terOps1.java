754092a0ab8f7c306896174de89286279a8badf7
Prabhat Kumar <pkkrsingh877@gmail.com>
2021-10-12 05:38:11 +0530

    added terOps 1

---
+++ terOps 1/terOps1.java
@@ -0,0 +1,6 @@
+public class terOps1{
+    public static void main(String args[]){
+        int age = 20;
+        (age > 18)?System.out.print("You are an adult."):System.out.print("You are not an adult.");
+    }     
+}
