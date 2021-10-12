aecfbfef7fdb8871e6dd236462755a7ed31393cb
Prabhat Kumar <pkkrsingh877@gmail.com>
2021-10-12 05:39:09 +0530

    added vid

---
+++ vid/vid.java
@@ -0,0 +1,11 @@
+//Now we write the actual program.
+public class vid{
+    public static void main(String[] args){
+        int a;
+        int b;
+        a = 20;
+        b = 30;
+        int c = a + b;
+        System.out.println("Sum of the two numbers is "+c); 
+    }
+}
